f91900b0bc004421b5bb1bbe14ccde4c64fbb6f6
eclipse_user <eclipse_user@atguigu.com>
2021-04-15 14:25:55 +0800

    edit on branch hot_fix

--- src/main/java/com/atguigu/git/TestGit/Happy.java
+++ src/main/java/com/atguigu/git/TestGit/Happy.java
@@ -4,6 +4,7 @@ public class Happy {
 	public static void main(String[] args){
 		System.out.println("Test modify");
 		System.out.println("Test second");
+		System.out.println("hot_fix 分支");
 	}
 
 }
